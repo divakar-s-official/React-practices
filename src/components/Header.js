dc0a9d50b7d4ca24516e57e51ad13f00e1d41134
divakar-s-official <divaoffi01@gmail.com>
2024-08-01 18:40:10 +0530

    applied search functionality and created a clone of listOfRestaurants

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LOGO from '../../public/LOGO.png'
 
 const Header = () => {
+  let [btnName,setBtnName] = useState("Login");
+  console.log("header rendered");
   return (
-    <div className='flex items-center justify-between shadow-lg px-8 '>
+    <div className='flex items-center justify-between px-8 shadow-lg '>
         <div>
             <img src={LOGO} className='w-36'/>
         </div>
@@ -13,6 +15,7 @@ const Header = () => {
                 <li className='p-6 '>About</li>
                 <li className='p-6 '>Contact</li>
                 <li className='p-6 '>Cart</li>
+                <button onClick={()=>btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")}>{btnName}</button>
             </ul>
         </div>
         
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -6,6 +6,8 @@ import ShimmerUI from './ShimmerUI'
 
 const Body = () => {
   const [listOfRestaurants,setListOfRestaurants] = useState([])
+  const [filteredListOfRestaurants,setFilteredListOfRestaurants] = useState([])
+  const [searchText,setSearchText] = useState("");
 
   
   
@@ -17,30 +19,44 @@ const Body = () => {
   const fetchData = async() => {
     const data = await fetch(API_URL);
     const json = await data.json();
-    console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     //optional chaining
     setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
   
 
   
   
     
-  console.log('body rendered');
-  return listOfRestaurants===0 ? <ShimmerUI /> : (
+  
+  return listOfRestaurants.length ===0 ? (<ShimmerUI />) : (
     <div className='body'>
-    <div className='px-10 mt-5 mb-4'>
+    <div className='flex gap-2 px-10 mt-5 mb-4'>
+      <div>
+        <input type='text' className='border border-black' value={searchText} onChange={(event)=>setSearchText(event.target.value)}/>
+        <button className='p-2 ml-2 bg-gray-300 rounded-lg cursor-pointer' 
+        onClick={()=>{
+          //filter the restaurant cards and update the UI
+          //searchText
+          console.log(searchText);
+          const filteredRestaurant = listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText));
+          setFilteredListOfRestaurants(filteredRestaurant);
+        }}
+        >Search</button>
+      </div>  
+      
       <button className='p-2 bg-gray-300 rounded-lg cursor-pointer' 
         onClick={()=> {
           const filteredRestaurants = listOfRestaurants.filter((restaurant)=>restaurant.info.avgRatingString >= 4.5)
           //whenever my state variable changes react re-render a component
           setListOfRestaurants(filteredRestaurants)
         }}
-      >Top Rated Restaurants</button>  
+      >Top Rated Restaurants</button>
     </div>   
     <div className='flex flex-wrap justify-start px-8 '>
-        {listOfRestaurants.map((restaurants)=>(
+        {filteredListOfRestaurants.map((restaurants)=>(
             <RestaurantCard  resdata={restaurants.info} key={restaurants.info.name}/>
         ))}
         
