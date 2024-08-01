b52aa56139b89cbe4324d92e33b914a1c21747d2
divakar-s-official <divaoffi01@gmail.com>
2024-08-01 10:30:38 +0530

    fetch the actual swiggy  API data over here as well as useEffect() used over here

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ import { useState } from "react"
 export const LOGO_URL = 'https://www.google.com/imgres?q=food%20app%20logo%20design&imgurl=https%3A%2F%2Fwww.logodesign.net%2Flogo%2Fsmoking-burger-with-lettuce-3624ld.png%3Fnwm%3D1%26nws%3D1%26industry%3Dfast-food%26sf%3D%26txt_keyword%3DAll&imgrefurl=https%3A%2F%2Fwww.logodesign.net%2Flogos%2Ffast-food&docid=7dHlwPJk-MkDvM&tbnid=hXdeg1sHFMWVNM&vet=12ahUKEwjJ34rHtdCHAxVzyjgGHavBGPYQM3oECBgQAA..i&w=405&h=309&hcb=2&ved=2ahUKEwjJ34rHtdCHAxVzyjgGHavBGPYQM3oECBgQAA'
 
 export const CDN_LOGO = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
+
+export const API_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
